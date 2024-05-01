ef7c55da9cdab07a85a5e9cc9509deb6df246604
nehalahmedshaikh <nehalahmedshaikh@gmail.com>
2024-05-02 00:19:27 +0500

    updated scripts

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ downloadCVButton.addEventListener('click', function(event) {
     // Check if Ctrl (or Cmd on Mac) is pressed
     if (event.ctrlKey || event.metaKey) {
         // Open the link in a new tab
-        window.open('nehalahmedshaikh.github.io/nehal_cv.pdf', '_blank');
+        window.open('https://nehalahmedshaikh.github.io/nehal_cv.pdf', '_blank');
     } else {
         // Redirect in the same tab
-        window.location.href = 'nehalahmedshaikh.github.io/nehal_cv.pdf';
+        window.location.href = 'https://nehalahmedshaikh.github.io/nehal_cv.pdf';
     }    
 });
 
