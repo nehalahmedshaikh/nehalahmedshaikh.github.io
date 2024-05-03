087390f321fdb7e964bb400c2e91a4fd13688bcc
nehalahmedshaikh <nehalahmedshaikh@gmail.com>
2024-05-03 21:04:04 +0500

    updated scripts

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // script.js
 
-// Add a click event listener to the button
-downloadCVButton.addEventListener('click', function(event) {
-    // Check if Ctrl (or Cmd on Mac) is pressed
-    if (event.ctrlKey || event.metaKey) {
-        // Open the link in a new tab
-        window.open('https://nehalahmedshaikh.github.io/nehal_cv.pdf', '_blank');
-    } else {
-        // Redirect in the same tab
-        window.location.href = 'https://nehalahmedshaikh.github.io/nehal_cv.pdf';
-    }    
-});
+// // Add a click event listener to the button
+// downloadCVButton.addEventListener('click', function(event) {
+//     // Check if Ctrl (or Cmd on Mac) is pressed
+//     if (event.ctrlKey || event.metaKey) {
+//         // Open the link in a new tab
+//         window.open('https://nehalahmedshaikh.github.io/nehal_cv.pdf', '_blank');
+//     } else {
+//         // Redirect in the same tab
+//         window.location.href = 'https://nehalahmedshaikh.github.io/nehal_cv.pdf';
+//     }    
+// });
 
-// Add a click event listener to the button
-githubButton.addEventListener('click', function(event) {
-    // Check if Ctrl (or Cmd on Mac) is pressed
-    if (event.ctrlKey || event.metaKey) {
-        // Open the link in a new tab
-        window.open('https://github.com/nehalahmedshaikh', '_blank');
-    } else {
-        // Redirect in the same tab
-        window.location.href = 'https://github.com/nehalahmedshaikh';
-    }
-});
+// // Add a click event listener to the button
+// githubButton.addEventListener('click', function(event) {
+//     // Check if Ctrl (or Cmd on Mac) is pressed
+//     if (event.ctrlKey || event.metaKey) {
+//         // Open the link in a new tab
+//         window.open('https://github.com/nehalahmedshaikh', '_blank');
+//     } else {
+//         // Redirect in the same tab
+//         window.location.href = 'https://github.com/nehalahmedshaikh';
+//     }
+// });
 
 const darkModeButton = document.getElementById("darkModeButton");
 let isDarkMode = false; // Initial state (light mode)
